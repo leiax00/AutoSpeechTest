791c1200f275aad47a062fd828672d54cc2e8580
leiax00 <leiax00@outlook.com>
2020-01-16 11:00:28 +0800

    增加保护

--- python/audio_identify/collector.py
+++ python/audio_identify/collector.py
@@ -16,24 +16,32 @@ class Collector(Receiver):
         self.__start = True
         self.com_device = com_device
         self.thread_name = '{0}:{1}'.format('collector', self.com_device)
-        self.serial = serial.Serial(port=self.com_device, baudrate=115200, timeout=0.5)
+        self.serial_com = None
         self.tmp_data = [format_time(), self.com_device]
 
     def run(self):
+        while self.serial_com is None:
+            try:
+                self.serial_com = serial.Serial(port=self.com_device, baudrate=115200, timeout=0.5)
+            except Exception as e:
+                logger.error('com connect exception, e:{0}'.format(e))
         self.listen()
 
     def listen(self):
-        if not self.serial.isOpen():
-            self.serial.open()
-
+        if not self.serial_com.isOpen():
+            self.serial_com.open()
+        logger.info('com connect success, com:{0}'.format(self.com_device))
         while self.__start:
-            line = self.serial.readline().decode(encoding='utf-8').strip('\r\n\t')
-            if line is not None and line != '' and not log_filter.need_filter(line):
-                self.tmp_data.append(line)
+            try:
+                line = self.serial_com.readline().decode(encoding='utf-8').strip('\r\n\t')
+                if line is not None and line != '' and not log_filter.need_filter(line):
+                    self.tmp_data.append(line)
+            except Exception as e:
+                logger.warn('com may may not read log, e:{0}'.format(e))
 
     def remove(self):
         self.__start = False
-        self.serial.close()
+        self.serial_com.close()
         logger.info('success to close thread, com:{0}'.format(self.com_device))
 
     def on_notify(self, *o):
