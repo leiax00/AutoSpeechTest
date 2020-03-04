e7c4baa11423f6a2378dd9dff21f91a632cf809d
leiax00 <leiax00@outlook.com>
2020-03-04 16:18:28 +0800

    独立播放脚本，增加sox音量增强

--- scripts/play_wav.py
+++ scripts/play_wav.py
@@ -4,20 +4,24 @@ import json
 import os
 import shutil
 import time
+import traceback
 import wave
 from time import sleep
 
-import numpy as np
 import pyaudio
-from scipy.io import wavfile
+import sox
 
 
 class _Player:
-    def play_wav(self, wav_name, voice_same=False):
-        if voice_same:
+    def __init__(self):
+        self.transformer = sox.Transformer()
+        self.root = os.path.dirname(__file__)
+
+    def play_wav(self, wav_name, need_voice_same=False):
+        if need_voice_same:
             tmp_file_path = self.deal_voice(wav_name)
         else:
-            tmp_file_path = shutil.copy(wav_name, os.path.dirname(__file__))
+            tmp_file_path = shutil.copy(wav_name, self.root)
         chunk = 1024
         p = pyaudio.PyAudio()
         wf = wave.open(tmp_file_path, 'rb')
@@ -37,13 +41,20 @@ class _Player:
         wf.close()
         os.remove(tmp_file_path)
 
-    @staticmethod
-    def deal_voice(filename):
-        dst_f = os.path.join(os.path.dirname(__file__), os.path.basename(filename))
-        wav_l = list(wavfile.read(filename))
-        normal_waves = np.array(wav_l[1] / np.max(wav_l[1]) * 32768, dtype=np.int16)
-        wavfile.write(dst_f, wav_l[0], normal_waves)
-        return dst_f
+    def deal_voice(self, filename):
+        dst_f = os.path.join(self.root, os.path.basename(filename))
+        try:
+            v_adjustment = float(self.transformer.stat(filename).get('Volume adjustment')) or 1.0
+            print('voice increment:{0}, wav_path:{1}'.format(v_adjustment, filename))
+            self.transformer.vol(v_adjustment)
+            self.transformer.build(filename, dst_f)
+            self.transformer.clear_effects()
+            return dst_f
+        except Exception as e:
+            print('Failed to adjust volume, use origin wav. err: {0}, {1}'.format(e, traceback.format_exc()))
+            if os.path.exists(dst_f):
+                os.remove(dst_f)
+            return shutil.copy(filename, self.root)
 
 
 def read_json(p):
@@ -73,7 +84,8 @@ def collect_log(com):
 
 if __name__ == '__main__':
     player = _Player()
-    default_input = r'\\U9wcu7c53reyco7\f\dev\tools\AutoTestTool\res\mt_penqiangyidang.json'
+    # default_input = r'D:\code\AutoSpeechTest\scripts\bad_wav.json'
+    default_input = r'\\192.168.1.8\corpus\project\modan_gtj\tts_test\tts_4_auto_test.json'
     print('请输入自动化测试结构的wav信息json文件, 不输入请修改py文件中的default_input的值。')
     in_p = input('json filepath:')
     if in_p is None or in_p == '':
@@ -86,7 +98,7 @@ if __name__ == '__main__':
                 content = cmd_o.get('content')
                 wav_p = cmd_o.get('source')
                 print(format_time_4_log(), ' start paly:aid:{0}, content:{1}, source:{2}'.format(aid, content, wav_p))
-                player.play_wav(wav_p)
+                player.play_wav(wav_p, True)
                 print(format_time_4_log(), ' play end, aid:{0}'.format(aid))
                 sleep(2)
                 print('*' * 10, ' next ', '*' * 10)
